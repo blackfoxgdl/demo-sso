f804f7101bbbb12119e8f1bc7e38e67785a6bbd8
blackfoxgdl <ruben.alonso21@gmail.com>
2024-06-03 18:19:23 -0600

    Issue with cors headers

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, content-type'
   );
   res.setHeader(
-    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Methods',
     'GET, POST, PUT, DELETE, PATCH, OPTIONS'
   );
   next();
